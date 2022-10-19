7b14a66ef546ce2d8529c74375ff16dc7f9e7eca
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-19 14:24:38 -0400

    code change

--- main.cs
+++ main.cs
@@ -11,18 +11,13 @@ class Program {
         // This function accepts user input
         int length;
         int area;
-
         Console.WriteLine("This program calculates the area and perimeter of a rectangle.");
         Console.WriteLine("");
-
         Console.Write("Enter the length (cm): ");
         length = Convert.ToInt32(Console.ReadLine());
-
         area = length * length;
-
         Console.WriteLine("");
         Console.WriteLine("The area is: " + area + " cm². ");
-
         Console.WriteLine("\nDone.");
     }
 }
